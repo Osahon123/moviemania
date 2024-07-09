74bf372c726a52ad08156e80ca9ddf2602744b02
Osahon123 <160333233+Osahon123@users.noreply.github.com>
2024-07-09 01:36:08 +0100

    Add files via upload

---
+++ autocomplete.js
@@ -0,0 +1,31 @@
+let availableKeywords = [
+    'A family Affair',
+    'Beverly Hills Cop',
+    'Trigger warning',
+    'Space cadet',
+    'Bosco',
+];
+
+const resultsBox = document.querySelector(".result-box");
+const inputBox = document.getElementById("input-box");
+
+
+inputBox.onkeyup = function() {
+    let result= [];
+    let input =inputBox.value;
+    if(input.length){
+        result = availableKeywords.filter((keyword)=> {
+            return keyword.toLowerCase().includes(input.toLowerCase());
+        });
+        console.log(result);
+    }
+    display(result)
+}
+
+function display(result){
+    const content = result.map((list)=>{
+        return "<li>" + list + "</li>";
+    });
+
+    resultsBox.innerHTML = "<ul>" + content.join('') + "</ul>";
+}
